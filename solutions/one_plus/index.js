5ed4d3b23435e4f0b00ddf6e9f3bdd8758b8ff0a
rabbygit <rabbyhossain482@gmail.com>
2021-07-30 12:00:18 +0600

    solution of one plus added in js

---
+++ solutions/one_plus/index.js
@@ -0,0 +1,26 @@
+/**
+ * @author Rabby hossain
+ * [Problem ref]{@link https://leetcode.com/problems/plus-one/}
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = function (digits) {
+    let hold = 1;
+
+    for (let index = digits.length - 1; index >= 0; index--) {
+        const sum = digits[index] + hold;
+        if (sum === 10) {
+            digits[index] = 0;
+            t = 1;
+        } else {
+            digits[index] = sum;
+            hold = 0;
+        }
+    }
+
+    if (hold) {
+        digits.unshift(hold)
+    }
+
+    return digits
+};
