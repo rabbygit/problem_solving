d1c689191df407537dcde8c5a62c2b5d2033b43e
rabbygit <rabbyhossain482@gmail.com>
2021-10-31 22:32:26 +0600

    in_order_traversal tree solution added

---
+++ solutions/tree/in_order_traversal/index.js
@@ -0,0 +1,48 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/binary-tree-inorder-traversal}
+ * @description Given the root of a binary tree, 
+ * return the inorder traversal of its nodes' values.
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const inorderTraversal = function (root) {
+    let result = [];
+    if (!root) return result;
+    return traverse(root, result);
+};
+
+/**
+ * @description traverse the tree in order(left-> root -> right)
+ * @param {*} root 
+ * @param {*} result 
+ * @returns {number[]}
+ */
+const traverse = (root, result) => {
+    // if root has not any node
+    if (!root) return;
+
+    // traverse left side
+    traverse(root.left, result);
+
+    // push the root in result
+    result.push(root.val);
+
+    // traverse the right side
+    traverse(root.right, result);
+
+    // return result array
+    return result;
+}
