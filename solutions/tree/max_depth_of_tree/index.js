99bcd3e8b35583f39296d42cc55a54a4e7e5b17b
rabbygit <rabbyhossain482@gmail.com>
2021-11-01 23:02:07 +0600

    max_depth_of_tree soltuion added

---
+++ solutions/tree/max_depth_of_tree/index.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function (root) {
+    // if root has not any node
+    if (!root) return 0;
+
+    let depth = 1;
+
+    // traverse tree and keep track of level
+    function traverse(root, level) {
+        // if root is null then return
+        if (!root) return;
+
+        // go left and increase level by 1
+        traverse(root.left, level + 1);
+
+        // check if current level is grater than depth
+        if (level > depth) {
+            depth = level;
+        }
+
+        // go right and increase level by 1
+        traverse(root.right, level + 1);
+    }
+
+    // call traverse from level 0
+    traverse(root, 1)
+
+    return depth;
+};
