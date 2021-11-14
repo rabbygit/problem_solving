c88843a599c025b3692428376630d6ff57189ead
rabbygit <rabbyhossain482@gmail.com>
2021-11-14 16:42:53 +0600

    path_sum solution added

--- solutions/tree/path_sum/index.js
+++ solutions/tree/path_sum/index.js
@@ -1,4 +1,11 @@
 /**
+ * @author Rabby Hossain
+ * [Problem ref]{@link https://leetcode.com/problems/path-sum/}
+ * @description Given the root of a binary tree and an integer targetSum, 
+ * return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ */
+
+/**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
@@ -11,6 +18,29 @@
  * @param {number} targetSum
  * @return {boolean}
  */
-var hasPathSum = function (root, targetSum) {
+const hasPathSum = function (root, targetSum) {
+  let result = false;
+  if (!root) return result;
 
+  function traverse(root, sum) {
+    if (!root) return;
+
+    // add the root value to sum
+    sum += root.val;
+
+    // move left
+    traverse(root.left, sum);
+
+    // check if root is leaf node(no child node) and sum is equal to targetSum
+    if (!root.left && !root.right && (sum === targetSum)) {
+      result = true;
+    }
+
+    // move right
+    traverse(root.right, sum)
+  }
+
+  traverse(root, 0)
+
+  return result;
 };
