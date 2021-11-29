bbc0711ddec655dc7128082dee43167823f376b5
rabbygit <rabbyhossain482@gmail.com>
2021-11-29 10:09:04 +0600

    solution added for construct_bst_from_preorder_and_postorder traversal array

--- solutions/tree/construct_bst_from_pre_post/index.js
+++ solutions/tree/construct_bst_from_pre_post/index.js
@@ -19,23 +19,21 @@ function TreeNode(val, left, right) {
  * @return {TreeNode}
  */
 const constructFromPrePost = function (preorder, postorder) {
-  console.log(preorder, postorder);
   if (!preorder.length || !postorder.length) {
     return null;
   }
 
   // construct the root from the first element of the preorder array
   let root = new TreeNode(preorder[0]);
+
   // find the root value from postorder
-  let index = postorder.findIndex(element => element === preorder[0])
+  const index = postorder.findIndex(element => element === preorder[1]);
 
   // construct the left sub tree
-  root.left = constructFromPrePost(preorder.slice(1, index + 1), postorder.slice(0, index + 1))
+  root.left = constructFromPrePost(preorder.slice(1, index + 2), postorder.slice(0, index + 1));
   // construct the right sub tree
-  root.right = constructFromPrePost(preorder.slice(index + 1), postorder.slice(index + 1))
+  root.right = constructFromPrePost(preorder.slice(index + 2), postorder.slice(index + 1, postorder.length - 1))
 
   // return the root
   return root;
 };
-
-console.log(constructFromPrePost([1, 2, 4, 5, 3, 6, 7], [4, 5, 2, 6, 7, 3, 1]));
