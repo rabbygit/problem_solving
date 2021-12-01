e610480af342e8d9d61671bd1bf98db6cfb8b2f9
rabbygit <rabbyhossain482@gmail.com>
2021-12-01 12:06:42 +0600

    solution for minimum depth of bst added

--- solutions/tree/max_depth_of_tree/index.js
+++ solutions/tree/max_depth_of_tree/index.js
@@ -1,4 +1,11 @@
 /**
+ * @author Rabby Hossain
+ * [Problem ref]{@link https://leetcode.com/problems/maximum-depth-of-binary-tree/}
+ * @description Given the root of a binary tree, return its maximum depth.
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
+
+/**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
