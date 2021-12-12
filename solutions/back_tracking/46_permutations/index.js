5f9054cb6b7bcbf349e15538c09aceeb6f5536c7
rabbygit <rabbyhossain482@gmail.com>
2021-12-12 19:09:18 +0600

    permutation solution added

---
+++ solutions/back_tracking/46_permutations/index.js
@@ -0,0 +1,38 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link https://leetcode.com/problems/permutations/}
+ * @description Given an array nums of distinct integers,
+ * return all the possible permutations. You can return the answer in any order.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = function (nums) {
+  const result = [];
+  const taken = {};
+  const n = nums.length;
+
+  function genratePermute(sub_result) {
+    if (sub_result.length === n) {
+      result.push([...sub_result]);
+      return;
+    }
+
+    for (let index = 0; index < n; index++) {
+      const element = nums[index];
+      if (!taken[element]) {
+        taken[element] = true;
+        sub_result.push(element);
+        genratePermute(sub_result);
+        sub_result.pop();
+        taken[element] = false;
+      }
+    }
+  }
+
+  genratePermute([]);
+
+  return result;
+};
