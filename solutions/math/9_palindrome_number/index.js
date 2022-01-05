27b4561228ee011ee80a41e1ad144f821cb486d0
rabbygit <rabbyhossain482@gmail.com>
2022-01-06 00:41:00 +0600

    palindrome_number check issue with negative number

---
+++ solutions/math/9_palindrome_number/index.js
@@ -0,0 +1,23 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link https://leetcode.com/problems/palindrome-number/}
+ * @description An integer is a palindrome when it reads the same backward as forward.
+ */
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function (x) {
+    // reverse the integer
+    let digit, result = 0, n = x
+    while (n) {
+        digit = n % 10  //  Get right-most digit. Ex. 123/10 → 12.3 → 3
+        result = (result * 10) + digit  //  Ex. 123 → 1230 + 4 → 1234
+        n = parseInt(n / 10) //  Remove right-most digit. Ex. 123 → 12.3 → 12
+    }
+
+    return result == x
+};
+
+console.log(isPalindrome(99));
