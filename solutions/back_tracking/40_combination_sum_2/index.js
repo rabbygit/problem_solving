19020e7affd112a8a83c4d605d4237c0863fbcd8
rabbygit <rabbyhossain482@gmail.com>
2022-01-13 16:51:08 +0600

    Add solution for 40_combinantion_sum_2 problem

--- solutions/back_tracking/40_combination_sum_2/index.js
+++ solutions/back_tracking/40_combination_sum_2/index.js
@@ -15,15 +15,10 @@
 const combinationSum2 = function (candidates, target) {
     const result = []
     const n = candidates.length
-    const element_map = {}
 
-    for (let index = 0; index < candidates.length; index++) {
-        const element = candidates[index];
-        if (element_map[element] === undefined) {
-            element_map[element] = 1
-        } else {
-            element_map[element]++
-        }
+    if (n > 1) {
+        // sort the elements to keep same element side by side like [1,2,3,1] to [1,1,2,3]
+        candidates = mergeSort(candidates, 0, n - 1);
     }
 
     function generate_combination(sub_result, sum, i) {
@@ -36,20 +31,88 @@ const combinationSum2 = function (candidates, target) {
             return
         }
 
+
         for (let index = i; index < n; index++) {
             const candidate = candidates[index]
+            // skip duplicate value and move to next element
+            if (i != index && candidate === candidates[index - 1]) {
+                continue
+            }
             sub_result.push(candidate)
-            sum += candidate
-            i++
-            generate_combination(sub_result, sum, i)
+            generate_combination(sub_result, sum + candidate, index + 1)
             sub_result.pop()
-            sum -= candidate
         }
     }
 
     generate_combination([], 0, 0)
-
     return result
 };
 
-console.log(combinationSum2([2, 5, 2, 1, 2], 5));
+function merge(arr, l, m, r) {
+    let n1 = m - l + 1;
+    let n2 = r - m;
+
+    // Create temp arrays
+    let L = new Array(n1);
+    let R = new Array(n2);
+
+    // Copy data to temp arrays L[] and R[]
+    for (let i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (let j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+
+    // Initial index of first subarray
+    let i = 0;
+
+    // Initial index of second subarray
+    let j = 0;
+
+    // Initial index of merged subarray
+    let k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    return arr
+}
+
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+function mergeSort(arr, l, r) {
+    if (l >= r) {
+        return;//returns recursively
+    }
+    let m = l + parseInt((r - l) / 2);
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    return merge(arr, l, m, r);
+}
