156ad5eaae39e622021c9f2297c188c7ff6a7a0b
rabbygit <rabbyhossain482@gmail.com>
2022-01-16 18:12:44 +0600

    Add improved solution to 'remove_duplicate_from_sorted_array' problem

--- solutions/array/remove_duplicate_from_sorted_array/index.js
+++ solutions/array/remove_duplicate_from_sorted_array/index.js
@@ -5,6 +5,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+// solution: 1
+// Runtime: 0(n2)
 const removeDuplicates = function (nums) {
     let k = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -19,5 +21,32 @@ const removeDuplicates = function (nums) {
             }
         }
     }
+    return k;
+};
+
+// solution: 2
+// Runtime: 0(n)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function (nums) {
+    let n = nums.length
+    if (!n) return 0;
+
+    let k = 1
+    let j = 1
+    let replace_idx = 1
+
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i] != nums[j]) {
+            k++
+            nums[replace_idx] = nums[j]
+            replace_idx++
+        }
+
+        j++
+    }
+
     return k;
 };
--- solutions/array/remove_duplicate_from_sorted_array/index.py
+++ solutions/array/remove_duplicate_from_sorted_array/index.py
@@ -3,24 +3,45 @@
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-      i = 0
-      k = 0 # for keep tracking of last unique element
-      length = len(nums)
+        i = 0
+        k = 0  # for keep tracking of last unique element
+        length = len(nums)
 
-      while i < length:
-          # assign new element
-          k+=1
-          nums[k-1] = nums[i];
+        while i < length:
+            # assign new element
+            k += 1
+            nums[k-1] = nums[i]
 
-          # loop through to find same elements
-          for j in range(i+1 , length):
-              if(nums[j] != nums[i]):
-                  break;
-              else:
-                  # same element and pass
-                  i+=1;
+            # loop through to find same elements
+            for j in range(i+1, length):
+                if(nums[j] != nums[i]):
+                    break
+                else:
+                    # same element and pass
+                    i += 1
 
-          # increment the counter
-          i+=1   
+            # increment the counter
+            i += 1
 
-      return k;
+        return k
+
+    # Imroved soltuion
+    # Runtime: 0(n)
+    def removeDuplicates2(self, nums: List[int]) -> int:
+        length = len(nums)
+        if length == 0:
+            return 0
+
+        j = 1
+        k = 1
+        replace_idx = 1
+
+        for i in range(length-1):
+            if nums[i] != nums[j]:
+                nums[replace_idx] = nums[j]
+                replace_idx += 1
+                k += 1
+
+            j += 1
+
+        return k
