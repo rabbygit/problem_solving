31163ead80ee73a1885cb29903702524d8f765bc
rabbygit <rabbyhossain482@gmail.com>
2022-02-03 15:30:59 +0600

    Add python BFS approach solution for '1971_find_if_path_exists' problem

--- graph/1971_find_if_path_exists/index.py
+++ graph/1971_find_if_path_exists/index.py
@@ -1,8 +1,12 @@
+from collections import deque
+
+
 class Solution:
+    # DFS approach
     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
         adjacency_list = [[] for _ in range(n)]
 
-        for a , b in edges:
+        for a, b in edges:
             adjacency_list[a].append(b)
             adjacency_list[b].append(a)
 
@@ -16,7 +20,7 @@ class Solution:
             # check if the node is our target node
             if node == end:
                 return True
-            
+
             # check if the node is already visited or not
             if node in seen:
                 continue
@@ -25,5 +29,32 @@ class Solution:
             # add all neighbor nodes of the node to stack
             for neighbor in adjacency_list[node]:
                 stack.append(neighbor)
+
+        return False
+
+    # BFS approach
+    def BFS(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        adjacency_list = [[] for _ in range(n)]
+
+        for a, b in edges:
+            adjacency_list[a].append(b)
+            adjacency_list[b].append(a)
+
+        queue = deque([start])
+        seen = set(start)
+
+        while queue:
+            # pop the first inserted node
+            node = queue.popleft()
+
+            # check if the node is our target node
+            if node == end:
+                return True
+
+            # add all neighbor nodes of the node to stack
+            for neighbor in adjacency_list[node]:
+                if not neighbor in seen:
+                    seen.add(neighbor)
+                    queue.append(neighbor)
 
         return False
