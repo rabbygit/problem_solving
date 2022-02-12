bf46b936515eaf8d1c48ee9f7d43e37b8aadb20b
rabbygit <rabbyhossain482@gmail.com>
2022-02-12 20:32:11 +0600

    Add js solution for '1290_convert_binary_to_integer' problem

---
+++ linked_list/1290_convert_binary_to_linked_list/index.js
@@ -0,0 +1,31 @@
+
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/}
+ * @description Given head which is a reference node to a singly-linked list. 
+ * The value of each node in the linked list is either 0 or 1. 
+ * The linked list holds the binary representation of a number.
+ * Return the decimal value of the number in the linked list.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+const getDecimalValue = function(head) {
+    let str = ''
+    while (head) {
+        str += head.val
+        head = head.next
+    }
+
+    return parseInt(str, 2)
+};
