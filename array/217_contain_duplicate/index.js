d1e884565444a107a777b41d9c10e8fbb35f0ed0
rabbygit <rabbyhossain482@gmail.com>
2022-02-14 15:24:11 +0600

    Add js solution for '217_contain_duplicate' problem

---
+++ array/217_contain_duplicate/index.js
@@ -0,0 +1,25 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/contains-duplicate/}
+ * @description Given an integer array nums, 
+ * return true if any value appears at least twice in the array, 
+ * and return false if every element is distinct.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = function(nums) {
+    const visited ={}
+
+    for (let index = 0; index < nums.length; index++) {
+      const element = nums[index];
+      if (visited[element]) {
+        return true
+      }
+      visited[element] = true
+    }
+
+    return false
+};
