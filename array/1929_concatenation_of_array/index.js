3dd2b1c4b93c95eb8f3e59f16e5ec1386ca876dc
rabbygit <rabbyhossain482@gmail.com>
2022-02-19 22:10:02 +0600

    Add js solution for 1929_concatenation_of_array

---
+++ array/1929_concatenation_of_array/index.js
@@ -0,0 +1,15 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/build-array-from-permutation/}
+ * @description Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and 
+ * ans[i + n] == nums[i] for 0 <= i < n (0-indexed).Specifically, ans is the concatenation of two nums arrays.
+ * Return the array ans.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const getConcatenation = function(nums) {
+    return [...nums,...nums]
+};
