de66a75b8d84a693bfc9c59c11eb43c0d34f6b3e
rabbygit <rabbyhossain482@gmail.com>
2022-02-25 10:24:30 +0600

    Add js and python solution for 1480_running_sum_in_1d_array

---
+++ array/1480_running_sum/index.js
@@ -0,0 +1,18 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/running-sum-of-1d-array/}
+ * @description Given an array nums. 
+ * We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const runningSum = function(nums) {
+    for (let index = 1; index < nums.length; index++) {
+        nums[index] = nums[index-1] + nums[index]
+    }
+
+    return nums
+};
