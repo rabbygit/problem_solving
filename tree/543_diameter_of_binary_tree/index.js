e0a1b8bd8f9f956d38ed3cf5b178d8ce214e7f1b
rabbygit <rabbyhossain482@gmail.com>
2022-02-26 23:45:34 +0600

    Add js and python solution for 543_diameter_of_binary_tree

---
+++ tree/543_diameter_of_binary_tree/index.js
@@ -0,0 +1,44 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/diameter-of-binary-tree/}
+ * @description Given the root of a binary tree, return the length of the diameter of the tree.
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. 
+ * This path may or may not pass through the root.
+ * The length of a path between two nodes is represented by the number of edges between them.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const diameterOfBinaryTree = function (root) {
+    let mps = Number.NEGATIVE_INFINITY;
+
+    function findMax(root) {
+        if (!root) return 0;
+
+        // choose the higest diamete of left sub-tree
+        let lps = Math.max(0, findMax(root.left));
+        // choose the higest diamete of right sub-tree
+        let rps = Math.max(0, findMax(root.right));
+        // keep track of the higest diamete, after comparing the previous highest diamete with
+        // combined value of current left , right
+        mps = Math.max(mps, lps + rps);
+
+        // propagate root+left or root+right nodes value to compare in upper stack call
+        return 1 + Math.max(lps, rps);
+    }
+
+    findMax(root);
+
+    return mps;
+};
