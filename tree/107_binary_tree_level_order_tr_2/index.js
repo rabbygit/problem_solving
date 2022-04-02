04dde3f672df985bf091bafc1abe829d04441f0a
rabbygit <rabbyhossain482@gmail.com>
2022-04-02 23:34:31 +0600

    Add js solution for '107_binary_tree_level_order_tr_2' problem

---
+++ tree/107_binary_tree_level_order_tr_2/index.js
@@ -0,0 +1,48 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/binary-tree-level-order-traversal-ii/}
+ * @description Given the root of a binary tree, 
+ * return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+ const levelOrderBottom = function (root) {
+    const level_map = {}
+    const result = []
+
+    function level_traverse(node, level) {
+        if (!node) return;
+
+        level_traverse(node.left, level + 1)
+
+        if (level_map[level] === undefined) {
+            level_map[level] = [node.val]
+        }else{
+            level_map[level].push(node.val)
+        }
+
+        level_traverse(node.right, level + 1)
+    }
+
+    level_traverse(root , 0)
+
+    const length = Object.entries(level_map).length - 1
+
+    for (let index = length; index >= 0; index--) {
+       result.push([...level_map[index]])
+    }
+
+    return result
+};
