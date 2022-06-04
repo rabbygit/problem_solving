3f6fe536420054144cabe02b59ad4fe4c547b404
rabbygit <rabbyhossain482@gmail.com>
2022-06-04 11:49:43 +0600

    Add js and python solution for 442_find_all_duplicates

---
+++ array/442_find_all_duplicates/index.js
@@ -0,0 +1,25 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link  https://leetcode.com/problems/find-all-duplicates-in-an-array/}
+ * @description Given an integer array nums of length n where all the integers of nums are in the range [1, n] and 
+ * each integer appears once or twice, return an array of all the integers that appears twice.
+ * You must write an algorithm that runs in O(n) time and uses only constant extra space
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const findDuplicates = function (nums) {
+    const result = []
+
+    for (let index = 0; index < nums.length; index++) {
+        let pos = Math.abs(nums[index]) - 1
+        if (nums[pos] < 0) {
+            result.push(pos + 1)
+        }
+        nums[pos] = -nums[pos]
+    }
+
+    return result
+};
