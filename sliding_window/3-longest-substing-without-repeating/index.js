93f51100625865ae3122f587a687d25a8e9191ea
rabbygit <rabbyhossain482@gmail.com>
2022-09-21 23:03:06 +0600

    solved 3-longest-substing-without-repeating

---
+++ sliding_window/3-longest-substing-without-repeating/index.js
@@ -0,0 +1,27 @@
+/**
+ * @author Rabby Hossain
+ * [Problem ref]{@link https://leetcode.com/problems/longest-substring-without-repeating-characters/}
+ * @description Given a string s, find the length of the longest substring without repeating characters.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function (s) {
+  let result = 0;
+  let left = 0;
+  const charSet = {};
+
+  for (let right = 0; right < s.length; right++) {
+    while (charSet[s[right]]) {
+      delete charSet[s[left]];
+      left++;
+    }
+
+    charSet[s[right]] = true;
+    result = Math.max(result, right - left + 1);
+  }
+
+  return result;
+};
---
+++ sliding_window/3-longest-substing-without-repeating/index.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        result = 0
+        left = 0
+        charSet = set()
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left +=1 
+            
+            charSet.add(s[right])
+            result = max(result, right - left + 1)
+
+        return result
