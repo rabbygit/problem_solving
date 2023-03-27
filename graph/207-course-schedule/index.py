49619eabd6c2cf7d07a93da08e79acca4b5699d4
rabbygit <rabbyhossain482@gmail.com>
2023-03-27 22:58:59 +0600

    added solution of 207-course-schedule

---
+++ graph/207-course-schedule/index.py
@@ -0,0 +1,42 @@
+import collections
+
+
+class Solution:
+    """ Kahn's algorithm for Topological Sorting
+        1. remove node which has zero indegree
+        2. decrease indegree value for all the dependend nodes of that removed node
+        3. Repeat
+    """
+
+    def canFinish(self, numCourses: int,
+                  prerequisites: List[List[int]]) -> bool:
+
+        adjacency_list = collections.defaultdict(list)
+        indegree = {}
+        q = collections.deque()
+
+        # create adjacency_list, calculate indegree
+        for src, des in prerequisites:
+            adjacency_list[src].append(des)
+            indegree[src] = indegree.get(src, 0)
+            indegree[des] = indegree.get(des, 0) + 1
+
+        # add all the nodes to queue that have zero indegree to process
+        for k, v in indegree.items():
+            if v == 0:
+                q.append(k)
+
+        count = 0
+        while q:
+            course = q.popleft()
+            count += 1
+
+            # decrease indegree value for all the dependend course
+            for dependend_course in adjacency_list[course]:
+                indegree[dependend_course] -= 1
+
+                # if the course has no indegree then add to the queue
+                if indegree[dependend_course] == 0:
+                    q.append(dependend_course)
+
+        return count == len(adjacency_list)
