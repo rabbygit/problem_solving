3cc5c9e1fca282e9ee8191eb8710bed794e14a68
rabbygit <rabbyhossain482@gmail.com>
2023-04-05 22:24:32 +0600

    modified solution of 1584_min_cost_to_connect_all_points

--- graph/1584_min_cost_to_connect_all_points/index.py
+++ graph/1584_min_cost_to_connect_all_points/index.py
@@ -120,3 +120,36 @@ class Solution:
                         heapq.heappush(priority_queue,Edge(point2 , j , distance))
 
         return min_cost
+
+    # using prim's algorithm
+    def minCostConnectPointsPrimAlt(self, points: List[List[int]]) -> int:
+        n = len(points)
+        adj = {i: [] for i in range(n)}
+        minHeap = [[0, 0]]  # cost, dst
+        totalCost = 0
+        visited = set()
+
+        # build the adjacency list with cost
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        # pop from the minHeap
+        # check if the node is in visited or not
+        # go through it's neighbors and add to minHeap
+        while len(visited) < n:
+            cost, i = heapq.heappop(minHeap)
+
+            if i not in visited:
+                totalCost += cost
+                visited.add(i)
+
+                for neiCost, nei in adj[i]:
+                    if nei not in visited:
+                        heapq.heappush(minHeap, [neiCost, nei])
+
+        return totalCost
