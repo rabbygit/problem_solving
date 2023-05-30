53c750b62a4ef405557f47aed81297535c07f55e
rabbygit <rabbyhossain482@gmail.com>
2023-05-30 20:53:11 +0600

    modified solution of 7-reverse-integer

--- bit_manipulation/7-reverse-integer/index.py
+++ bit_manipulation/7-reverse-integer/index.py
@@ -5,18 +5,18 @@ class Solution:
 
     def reverse(self, x: int) -> int:
         result = 0
-        MAX_INT = pow(2, 31) - 1  # 2147483647
-        MIN_INT = pow(-2, 31)  # -2147483648
+        MAX_INT_ONE_DIGIT_LESS = 214748364  #  MAX_INT -> 2147483647
+        MIN_INT_ONE_DIGIT_LESS = -214748364  # MIN_INT -> -2147483648
 
         while x:
             digit = int(math.fmod(x, 10))
             x = int(x / 10)
 
-            if (result > MAX_INT // 10) or (result == MAX_INT // 10
-                                            and digit > MAX_INT % 10):
+            if (result > MAX_INT_ONE_DIGIT_LESS) or (
+                    result == MAX_INT_ONE_DIGIT_LESS and digit > 7):
                 return 0
-            elif (result < MIN_INT // 10) or (result == MIN_INT // 10
-                                              and digit < MIN_INT % 10):
+            elif (result < MIN_INT_ONE_DIGIT_LESS) or (
+                    result == MIN_INT_ONE_DIGIT_LESS and digit < -8):
                 return 0
 
             result = (result * 10) + digit
