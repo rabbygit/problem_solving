569e0db76823e81543cd4746147cbeadec3394af
rabbygit <rabbyhossain482@gmail.com>
2023-06-11 21:35:28 +0600

    modified solution of 39-combination-sum

--- back_tracking/39-combination-sum/index.py
+++ back_tracking/39-combination-sum/index.py
@@ -1,23 +1,26 @@
+from typing import List
+
+
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+    def combinationSum(self, candidates: List[int],
+                       target: int) -> List[List[int]]:
         result = []
         n = len(candidates)
 
-        def genrate_combination(sub_result, sum, i):
+        def dfs(sub_result, sum, i):
             if sum == target:
                 result.append(sub_result.copy())
                 return
 
-            if sum > target:
+            if i >= n or sum > target:
                 return
 
-            for index in range(i, n):
-                candidate = candidates[index]
-                sub_result.append(candidate)
-                genrate_combination(sub_result, sum + candidate, i)
-                sub_result.pop()
-                i += 1
+            sub_result.append(candidates[i])
+            dfs(sub_result, sum + candidates[i], i)
+            sub_result.pop()
+            dfs(sub_result, sum, i + 1)
 
-        genrate_combination([], 0, 0)
+        dfs([], 0, 0)
 
         return result
