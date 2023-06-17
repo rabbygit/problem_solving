c5700b368ffc17d547d31f958bb0101490ff8f7e
rabbygit <rabbyhossain482@gmail.com>
2023-06-17 21:03:04 +0600

    added solution of 274-h-index

---
+++ binary-search/275-h-index-ii/index.py
@@ -0,0 +1,17 @@
+class Solution:
+    # https://www.youtube.com/watch?v=CjKJDloMnwE&t=744s&ab_channel=Techdose
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        l, r, mid = 0, n - 1, 0
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if citations[mid] == n - mid:
+                return citations[mid]
+            elif citations[mid] > n - mid:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return n - l
