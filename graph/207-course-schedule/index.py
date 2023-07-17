a0d3525f2aa6b1a4b14a9361ec0c011ef4710762
rabbygit <rabbyhossain482@gmail.com>
2023-07-17 22:25:17 +0600

    added dfs solution of 207-course-schedule

--- graph/207-course-schedule/index.py
+++ graph/207-course-schedule/index.py
@@ -40,3 +40,31 @@ class Solution:
                     q.append(dependend_course)
 
         return count == len(adjacency_list)
+
+    def canFinish1(self, numCourses: int,
+                   prerequisites: List[List[int]]) -> bool:
+        visited = set()
+        adjacency_list = collections.defaultdict(list)
+
+        # create adjacency_list
+        for crs, pre in prerequisites:
+            adjacency_list[crs].append(pre)
+
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if len(adjacency_list[crs]) == 0:
+                return True
+
+            visited.add(crs)
+            for pre in adjacency_list[crs]:
+                if not dfs(pre): return False
+
+            visited.remove(crs)
+            adjacency_list[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+
+        return True
