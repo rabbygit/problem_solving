8ba23b26ccdeaab656acbfaed75901e8c7b917a9
rabbygit <rabbyhossain482@gmail.com>
2023-07-20 22:17:20 +0600

    modified solution of 51-n-queen

--- back_tracking/51-n-queen/index.js
+++ back_tracking/51-n-queen/index.js
@@ -1,6 +1,6 @@
 /**
  * [Problem ref]{@link https://leetcode.com/problems/n-queens/}
- * @description The n-queens puzzle is the problem of placing n queens on 
+ * @description The n-queens puzzle is the problem of placing n queens on
  * an n x n chessboard such that no two queens attack each other.
  * Given an integer n, return all distinct solutions to the n-queens puzzle.
  * You may return the answer in any order.
@@ -11,80 +11,45 @@
  * @return {string[][]}
  */
 const solveNQueens = function (n) {
+  const board = [];
   const result = [];
-  const row_track = {}; // to track if queen is already placed in a specific row
-  const column_track = {}; // to track if queen is already placed in a specific column
-  const diagonal_track1 = {}; // to track if queen is already placed in lower to upper right diagonal
-  const diagonal_track2 = {}; // to track if queen is already placed in upper to lower right diagonal
 
-  function back_track(column, board) {
-    if (column === n) {
-      const sub_result = [];
+  // create n x n board
+  for (let i = 0; i < n; i++) board.push(new Array(n).fill("."));
+
+  const column = new Set(); // to track if queen is already placed in a specific column
+  const bottomToTop = new Set(); // to track if queen is already placed in lower left to upper right diagonal
+  const topToBottom = new Set(); // to track if queen is already placed in upper left to lower right diagonal
+
+  function back_track(r) {
+    if (r === n) {
       // construct one dimensional array
-      for (let index = 0; index < board.length; index++) {
-        const element = board[index];
-        sub_result.push(element.join(''));
-      }
-      // push sub_result to result
-      result.push(sub_result);
+      const c = [];
+      for (let i = 0; i < n; i++) c.push(board[i].join(""));
+      result.push(c);
       return;
     }
 
-    // traverse row wise
-    for (let row = 0; row < n; row++) {
+    for (let c = 0; c < n; c++) {
       // determine if queen can be placed at this position
-      if (canPlace(row, column, row_track, column_track, diagonal_track1, diagonal_track2)) {
-        // place the queen
-        board[row][column] = 'Q';
-
-        // keep track since queen can not be placed in same row/column/diagonal
-        row_track[row] = 'Q';
-        column_track[column] = 'Q';
-        diagonal_track1[row + column] = 'Q';
-        diagonal_track2[row - column] = 'Q';
-
-        // recurse for every column of the row
-        back_track(column + 1, board);
-
-        // back to previous state as queen can not be placed
-        board[row][column] = '.';
-        row_track[row] = '.';
-        column_track[column] = '.';
-        diagonal_track1[row + column] = '.';
-        diagonal_track2[row - column] = '.';
+      if (column.has(c) || bottomToTop.has(r + c) || topToBottom.has(r - c)) {
+        continue;
       }
+
+      board[r][c] = "Q";
+      column.add(c);
+      bottomToTop.add(r + c);
+      topToBottom.add(r - c);
+
+      back_track(r + 1);
+
+      board[r][c] = ".";
+      column.delete(c);
+      bottomToTop.delete(r + c);
+      topToBottom.delete(r - c);
     }
   }
 
-  // create n x n board
-  const board = [];
-  for (let index = 0; index < n; index++) {
-    board.push(new Array(n).fill('.'));
-  }
-
-  back_track(0, board);
-
-  // return result
+  back_track(0);
   return result;
-};
-
-/**
- * 
- * @description Determine if queen can be placed or not
- * @param {*} row
- * @param {*} column
- * @param {*} row_track
- * @param {*} column_track
- * @param {*} diagonal_track
- * @returns {Boolean}
- */
-const canPlace = function (row, column, row_track, column_track, diagonal_track1, diagonal_track2) {
-  if (row_track[row] === 'Q'
-    || column_track[column] === 'Q'
-    || diagonal_track1[row + column] === 'Q'
-    || diagonal_track2[row - column] === 'Q') {
-    return false;
-  }
-
-  return true;
 };
