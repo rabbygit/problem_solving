3d57bcc26334b2f61cada1b2f02f35ed61693e7b
rabbygit <rabbyhossain482@gmail.com>
2023-07-25 21:43:27 +0600

    modified solution of 22-generate-parentheses

--- back_tracking/22-generate-parentheses/index.js
+++ back_tracking/22-generate-parentheses/index.js
@@ -1,6 +1,6 @@
 /**
  * [Problem ref]{@link https://leetcode.com/problems/generate-parentheses/}
- * @description Given n pairs of parentheses, 
+ * @description Given n pairs of parentheses,
  * write a function to generate all combinations of well-formed parentheses.
  */
 
@@ -9,34 +9,23 @@
  * @return {string[]}
  */
 const generateParenthesis = function (n) {
-  const result = []
+  const result = [];
 
   function generate(l, r, sub_result) {
     // opening and closing parentheis are all used
-    if (!l && !r) {
+    if (l === n && r === n) {
       result.push(sub_result);
       return;
     }
 
-    // go further when remaining opening parenthesis count is greater
-    if (l > r) {
-      return
-    }
-
     // add opening parenthesis if remains
-    if (l) {
-      generate(l - 1, r, sub_result + '(');
-    }
+    if (l < n) generate(l + 1, r, sub_result + "(");
 
-    // add closing parenthesis if remains
-    if (r) {
-      generate(l, r - 1, sub_result + ')');
-    }
+    // add closing parenthesis if we have more open parentheses
+    if (l > r) generate(l, r + 1, sub_result + ")");
   }
 
-  generate(n, n, '');
+  generate(0, 0, "");
 
   return result;
 };
-
-console.log(generateParenthesis(3));
