f1a7ba23a938381b3b660c7f45ee9cb12e853498
rabbygit <rabbyhossain482@gmail.com>
2023-08-04 11:50:24 +0600

    added solution of 1992-find-all-groups-of-farmland

---
+++ back_tracking/1992-find-all-groups-of-farmland/index.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+
+    def findFarmland(self, land: List[List[int]]) -> int:
+        result = []
+        m, n = len(land), len(land[0])
+        bottomRight = []  # [i, j]
+
+        def dfs(r, c):
+            if r >= m or r < 0 or c >= n or c < 0 or land[r][c] == 0:
+                return
+
+            # farmland will be always rectangular
+            bottomRight[0] = max(bottomRight[0], r)
+            bottomRight[1] = max(bottomRight[1], c)
+
+            land[r][c] = 0
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for i in range(m):
+            for j in range(n):
+                if land[i][j] == 1:
+                    bottomRight = [i, j]
+                    dfs(i, j)
+                    result.append([i, j, bottomRight[0], bottomRight[1]])
+
+        return result
