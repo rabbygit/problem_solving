21efe1163dfdb48651b93448caf0335b4ba7a164
rabbygit <rabbyhossain482@gmail.com>
2023-08-18 21:36:34 +0600

    added solution of 1005-maximize-sum-of-array-after-k-negations

---
+++ array/1005-maximize-sum-of-array-after-k-negations/index.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        for i in range(len(nums)):
+            if nums[i] < 0 and k > 0:
+                nums[i] = -nums[i]
+                k -= 1
+
+        if k > 0 and k % 2 != 0:
+            nums.sort()
+            nums[0] = -nums[0]
+
+        return sum(nums)
