980d8f88dc1d7e7eb75716ccd1bed4f9c94dfbd5
rabbygit <rabbyhossain482@gmail.com>
2023-09-14 22:10:18 +0600

    added js solution of 844-backspace-string-compare

---
+++ string/844-backspace-string-compare/index.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceCompare = function (s, t) {
+  let i = s.length - 1;
+  let j = t.length - 1;
+  let skipS = 0;
+  let skipT = 0;
+
+  while (i >= 0 || j >= 0) {
+    while (i >= 0) {
+      if (s[i] === "#") {
+        skipS++;
+        i--;
+      } else if (skipS > 0) {
+        skipS--;
+        i--;
+      } else {
+        break;
+      }
+    }
+
+    while (j >= 0) {
+      if (t[j] === "#") {
+        skipT++;
+        j--;
+      } else if (skipT > 0) {
+        skipT--;
+        j--;
+      } else {
+        break;
+      }
+    }
+
+    if (i >= 0 && j >= 0 && s[i] !== t[j]) {
+      return false;
+    } else if (i >= 0 !== j >= 0) {
+      return false;
+    }
+
+    i--;
+    j--;
+  }
+
+  return true;
+};
