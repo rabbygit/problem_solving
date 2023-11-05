fab8aaec2619f77f0dfb0b791f732abbfa4a3368
rabbygit <rabbyhossain482@gmail.com>
2023-11-05 21:58:13 +0600

    feat: added solution of 935-knight-dialer

---
+++ math/935-knight-dialer/index.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    ref: https://www.youtube.com/watch?v=wRF40TQUpxg
+    ref: https://leetcode.com/problems/knight-dialer/solutions/189252/o-logn/
+    """
+
+    def knightDialer(self, N):
+        # intial step
+        x1 = x2 = x3 = x4 = x5 = x6 = x7 = x8 = x9 = x0 = 1
+
+        for _ in range(N - 1):
+            x1, x2, x3, x4, x5, x6, x7, x8, x9, x0 = \
+                x6 + x8, x7 + x9, x4 + x8, \
+                x3 + x9 + x0, 0, x1 + x7 + x0, \
+                x2 + x6, x1 + x3, x2 + x4, \
+                x4 + x6
+
+        return (x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x0) % (10**9 + 7)
