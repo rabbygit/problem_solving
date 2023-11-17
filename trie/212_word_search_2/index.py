af18717b539d422f0376e82929e2e741dcadd72b
rabbygit <rabbyhossain482@gmail.com>
2023-11-17 22:46:50 +0600

    fix: updated python solution of 212_word_search_2

--- trie/212_word_search_2/index.py
+++ trie/212_word_search_2/index.py
@@ -1,75 +1,70 @@
-
 class Node:
-    def __init__(self, letter):
-        self.letter = letter
+
+    def __init__(self):
         self.isWord = False
-        self.letter_map = {}
+        self.children = {}
 
 
 class Trie:
 
     def __init__(self):
-        self.root = Node('#')
+        self.root = Node()
 
     def insert(self, word: str) -> None:
-        current_node = self.root
+        curr = self.root
 
-        for letter in word:
-            if letter not in current_node.letter_map:
-                current_node.letter_map[letter] = Node(letter)
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = Node()
+            curr = curr.children[char]
 
-            current_node = current_node.letter_map[letter]
-
-        current_node.isWord = True
+        curr.isWord = True
 
     def findWords(self, board):
-        total_row = len(board)
-        total_column = len(board[0])
-        result = []
-        result_map = set()
+        rows, cols = len(board), len(board[0])
+        result = set()
 
-        def backtrack(row, column, sub_result, current_root, visited):
-            letter = board[row][column]
-            if not current_root or letter not in current_root.letter_map or (row, column) in visited:
+        def backtrack(r, c, sub_result, node):
+            letter = board[r][c]
+            if not node or letter not in node.children or board[r][c] == '#':
                 return
 
-            visited.add((row, column))  # track the position as visited
-            current_root = current_root.letter_map[letter]
+            # track the position as visited
+            tmp = board[r][c]
+            board[r][c] = '#'
+            node = node.children[letter]
 
-            # If it is the end of a word
-            if current_root.isWord and (sub_result+letter) not in result_map:
-                result_map.add(sub_result+letter)  # take only unique solution
-                result.append(sub_result+letter)
+            # it's the end of a word and take only unique solution
+            if node and node.isWord:
+                result.add(sub_result + letter)
 
-            #  maximum 4 possible path can be possible for a position and we need to go every possible path
-            if (column < total_column - 1):
-                backtrack(row, column + 1, sub_result +
-                          letter, current_root, visited)
-            if (column > 0):
-                backtrack(row, column - 1, sub_result +
-                          letter, current_root, visited)
-            if (row < total_row - 1):
-                backtrack(row + 1, column, sub_result +
-                          letter, current_root, visited)
-            if (row > 0):
-                backtrack(row - 1, column, sub_result +
-                          letter, current_root, visited)
+            # maximum 4 possible path can be possible for a position
+            # and we need to go every possible path
+            if (c < cols - 1):
+                backtrack(r, c + 1, sub_result + letter, node)
+            if (c > 0):
+                backtrack(r, c - 1, sub_result + letter, node)
+            if (r < rows - 1):
+                backtrack(r + 1, c, sub_result + letter, node)
+            if (r > 0):
+                backtrack(r - 1, c, sub_result + letter, node)
 
-            visited.remove((row, column))
+            board[r][c] = tmp
 
         # go through every single position and try all possiblities
-        for i in range(total_row):
-            for j in range(total_column):
-                backtrack(i, j, '', self.root, set())
+        for i in range(rows):
+            for j in range(cols):
+                backtrack(i, j, '', self.root)
 
-        return result
+        return list(result)
 
 
 class Solution:
+
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         trie = Trie()
 
-        # add all word to trie
+        # add all words to the trie data structure
         for word in words:
             trie.insert(word)
 
