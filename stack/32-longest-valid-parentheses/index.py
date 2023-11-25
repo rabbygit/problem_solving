d30a03e8bcce1231ccd51cf93d55e1d627a88f83
rabbygit <rabbyhossain482@gmail.com>
2023-11-25 22:11:33 +0600

    feat: added O(1) space solution of 32-longest-valid-parentheses

--- stack/32-longest-valid-parentheses/index.py
+++ stack/32-longest-valid-parentheses/index.py
@@ -17,3 +17,30 @@ class Solution:
                     result = max(result, i - stack[-1])
 
         return result
+
+    def longestValidParentheses2(self, s: str) -> int:
+        maxLen = l = r = 0
+
+        for c in s:
+            if c == '(': l += 1
+            else: r += 1
+
+            if l == r:
+                maxLen = max(maxLen, l * 2)
+            elif r > l:
+                l = r = 0
+
+        l = r = 0
+        # "(()"
+        for i in range(len(s) - 1, -1, -1):
+            c = s[i]
+
+            if c == '(': l += 1
+            else: r += 1
+
+            if l == r:
+                maxLen = max(maxLen, l * 2)
+            elif l > r:
+                l = r = 0
+
+        return maxLen
