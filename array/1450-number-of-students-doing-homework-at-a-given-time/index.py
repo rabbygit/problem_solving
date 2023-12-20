399d8ef761f469c5dd1ee91d87505e28bf826811
rabbygit <rabbyhossain482@gmail.com>
2023-12-20 20:05:10 +0600

    feat: added python solution of 1450-number-of-students-doing-homework-at-a-given-time

---
+++ array/1450-number-of-students-doing-homework-at-a-given-time/index.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def busyStudent(self, startTime: List[int], endTime: List[int],
+                    queryTime: int) -> int:
+        res = 0
+
+        for s, e in zip(startTime, endTime):
+            if s <= queryTime <= e:
+                res += 1
+
+        return res
