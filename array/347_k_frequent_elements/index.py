5b72c0c9abfc73911e2b2f4d8e31872a1bda4664
rabbygit <rabbyhossain482@gmail.com>
2023-12-21 15:56:43 +0600

    fix: modified python solution of 347_k_frequent_elements

--- array/347_k_frequent_elements/index.py
+++ array/347_k_frequent_elements/index.py
@@ -1,24 +1,22 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        count_map = {}
-        count_array = [[] for i in range(n+1)]
-        result = []
+import collections
+from typing import List
 
-        # count occurence for each element and keep in hashMap like 1 -> 4
-        for num in nums:
-            count_map[num] = 1 + count_map.get(num, 0)
+
+class Solution:
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        n, result = len(nums), []
+        count_map = collections.Counter(nums)
+        count_array = [[] for i in range(n + 1)]
 
         # build array for occurence from hashMap. like [ [] , [] , [] , [], [1,2 ..] ]
         for key, value in count_map.items():
             count_array[value].append(key)
 
         # build the result
-        for i in range(n+1 , 0, -1):
+        for i in range(n, 0, -1):
             for num in count_array[i]:
                 result.append(num)
                 # check k element is taken already or not
                 if len(result) == k:
                     return result
-        
-
