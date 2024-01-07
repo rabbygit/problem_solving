68609ecd3e9786d2ba8ae01cb4142ec4abc19c5f
rabbygit <rabbyhossain482@gmail.com>
2024-01-07 09:55:38 +0600

    feat: added python solution of 300-longest-increasing-subsequence

--- dp/300-longest-increasing-subsequence/index.py
+++ dp/300-longest-increasing-subsequence/index.py
@@ -1,3 +1,7 @@
+import bisect
+from typing import List
+
+
 class Solution:
 
     def lengthOfLIS(self, nums: List[int]) -> int:
@@ -9,3 +13,17 @@ class Solution:
                     dp[i] = max(dp[i], 1 + dp[j])
 
         return max(dp)
+
+# T.C: O(n logn)
+# S.C: O(n)
+class Solution1:
+    # ref: https://leetcode.com/problems/longest-increasing-subsequence/solutions/4509317/greedy-with-binary-search-beats-99-33-users/
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis = [nums[0]]
+        for x in nums[1:]:
+            if x > lis[-1]:
+                lis.append(x)
+            else:
+                i = bisect.bisect_left(lis, x)
+                lis[i] = x
+        return len(lis)
