dec797dd7b394b0be89d708d08ffe0d8599cbdcd
rabbygit <rabbyhossain482@gmail.com>
2024-01-10 22:48:32 +0600

    feat: added recursive python solution of 746-min-cost-climbing-stairs

--- dp/746-min-cost-climbing-stairs/index.py
+++ dp/746-min-cost-climbing-stairs/index.py
@@ -10,3 +10,21 @@ class Solution:
             cost[i] += min(cost[i + 1], cost[i + 2])
 
         return min(cost[0], cost[1])
+
+
+class Solution1:
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cache = {}
+
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            elif i in cache:
+                return cache[i]
+
+            cache[i] = min(cost[i] + dfs(i + 1), cost[i] + dfs(i + 2))
+            return cache[i]
+
+        dfs(0)
+        return min(cache[0], cache[1])
