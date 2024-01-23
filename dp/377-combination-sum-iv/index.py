6723d90d8949028c52c55dc1ae2be9d14486982e
rabbygit <rabbyhossain482@gmail.com>
2024-01-23 23:51:27 +0600

    feat: added python solution of 377-combination-sum-iv

---
+++ dp/377-combination-sum-iv/index.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    # similar to coin change
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for t in range(1, target + 1):
+            for n in nums:
+                if t - n >= 0:
+                    dp[t] += dp[t - n]
+
+        return dp[target]
