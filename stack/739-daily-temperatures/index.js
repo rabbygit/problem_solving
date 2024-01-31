a2b82223fa3ebecfdb4ff95c92f2f58d12c56447
rabbygit <rabbyhossain482@gmail.com>
2024-02-01 00:15:35 +0600

    fix: modified js solution of 739-daily-temperatures

--- stack/739-daily-temperatures/index.js
+++ stack/739-daily-temperatures/index.js
@@ -3,20 +3,18 @@
  * @return {number[]}
  */
 const dailyTemperatures = function (temperatures) {
+  const stack = [];
   const n = temperatures.length;
   const res = new Array(n).fill(0);
-  const stack = [[temperatures[n - 1], n - 1]];
 
-  for (let idx = n - 2; idx >= 0; idx--) {
-    while (stack.length && stack[stack.length - 1][0] <= temperatures[idx]) {
-      stack.pop();
+  for (let i = 0; i < n; i++) {
+    const temperature = temperatures[i];
+    while (stack.length && stack[stack.length - 1][1] < temperature) {
+      const [idx, _] = stack.pop();
+      res[idx] = i - idx;
     }
 
-    if (stack.length) {
-      res[idx] = stack[stack.length - 1][1] - idx;
-    }
-
-    stack.push([temperatures[idx], idx]);
+    stack.push([i, temperature]);
   }
 
   return res;
