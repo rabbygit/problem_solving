bd9cd63c13f42b2842e9712fa8a9647c338da236
rabbygit <rabbyhossain482@gmail.com>
2024-02-06 10:21:10 +0600

    fix: modified solution of 49-group-anargrams

--- array/49-group-anargrams/index.py
+++ array/49-group-anargrams/index.py
@@ -1,6 +1,10 @@
+import collections
+from typing import List
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anargrams_map = {}
+        anargrams_map = collections.defaultdict(list)
 
         for word in strs:
             char_count = [0] * 26
@@ -9,10 +13,6 @@ class Solution:
                 char_count[ord(char) - 97] +=  1
 
             anargram_key = '#'.join(map(str , char_count))
-
-            if anargram_key in anargrams_map:
-                anargrams_map[anargram_key].append(word)
-            else:
-                anargrams_map[anargram_key] = [word]
+            anargrams_map[anargram_key].append(word)
 
         return anargrams_map.values()
