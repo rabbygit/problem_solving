7630b0e6c7a6f235262c0bd7e6b9fac8d825a482
rabbygit <rabbyhossain482@gmail.com>
2024-02-08 22:43:53 +0600

    feat: added recursive solution of 279-perfect-squares

--- dp/279-perfect-squares/index.py
+++ dp/279-perfect-squares/index.py
@@ -15,3 +15,26 @@ class Solution:
                     dp[target] = min(dp[target], 1 + dp[target - square])
 
         return dp[n]
+
+
+class Solution1:
+
+    def numSquares(self, n: int) -> int:
+        maxSquare = int(math.sqrt(n))
+        cache = {}
+        cache[0] = 0
+
+        def dfs(target):
+            if target in cache:
+                return cache[target]
+            
+            res = n
+            for s in range(1, maxSquare + 1):
+                sq = s * s
+                if target - sq >= 0:
+                    res = min(res, 1 + dfs(target - sq))
+
+            cache[target] = res
+            return cache[target]
+
+        return dfs(n)
