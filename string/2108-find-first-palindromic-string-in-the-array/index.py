377e795a25c418f0043a5d1435b6e351aaf12b27
rabbygit <rabbyhossain482@gmail.com>
2024-02-13 21:36:54 +0600

    feat: added python solution of 2108-find-first-palindromic-string-in-the-array

---
+++ string/2108-find-first-palindromic-string-in-the-array/index.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            if self.isPalindrome(word):
+                return word
+
+        return ""
+
+    def isPalindrome(self, word: str) -> bool:
+        l, r = 0, len(word) - 1
+
+        while l <= r:
+            if word[l] != word[r]:
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
