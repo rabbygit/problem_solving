b28568a3135d2f4f30ab588a19125673580212a8
rabbygit <rabbyhossain482@gmail.com>
2024-02-13 22:59:33 +0600

    feat: added python solution of 929-unique-email-addresses

---
+++ string/929-unique-email-addresses/index.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique = set()
+
+        for e in emails:
+            local, domain = e.split('@')
+            local = local.split('+')[0].replace('.', '')
+            unique.add(local+'@'+domain)
+
+        return len(unique)
