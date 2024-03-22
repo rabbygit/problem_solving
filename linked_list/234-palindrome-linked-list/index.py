481c56ca0dfa4e1cd98283422f96e9f5d6fdd9f9
rabbygit <rabbyhossain482@gmail.com>
2024-03-22 16:50:44 +0600

    fix: modified solution of 234-palindrome-linked-list

--- linked_list/234-palindrome-linked-list/index.py
+++ linked_list/234-palindrome-linked-list/index.py
@@ -1,26 +1,21 @@
+from typing import Optional
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if head is None or head.next is None:
-            return True
-
-        slow = head
-        fast = head
+        slow = fast = head
         reversed = None
-        
+
         # find the middle of the list
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        # set None to the left half
-        reversed = slow
-        slow = slow.next
-        reversed.next = None
 
         # reverse the right half
         while slow:
@@ -30,11 +25,11 @@ class Solution:
             slow = temp
 
         # compare two halves of the list
-        while head and reversed:
+        while reversed:
             if head.val != reversed.val:
                 return False
             else:
                 head = head.next
                 reversed = reversed.next
-        
+
         return True
