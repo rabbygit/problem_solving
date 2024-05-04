d5561658771cc2b3f69832db26506374613b3bb0
rabbygit <rabbyhossain482@gmail.com>
2024-05-04 10:08:53 +0600

    fix: modified solution of 165-compare-version-numbers

--- string/165-compare-version-numbers/index.js
+++ string/165-compare-version-numbers/index.js
@@ -9,29 +9,18 @@ const compareVersion = function (version1, version2) {
 
   let l1 = 0;
   let l2 = 0;
-  while (l1 < v1Array.length && l2 < v2Array.length) {
-    if (v1Array[l1] > v2Array[l2]) {
+  while (l1 < v1Array.length || l2 < v2Array.length) {
+    const v1Value = l1 < v1Array.length ? v1Array[l1] : 0;
+    const v2Value = l2 < v2Array.length ? v2Array[l2] : 0;
+
+    if (v1Value > v2Value) {
       return 1;
-    } else if (v2Array[l2] > v1Array[l1]) {
+    } else if (v2Value > v1Value) {
       return -1;
     }
 
     l1++;
     l2++;
-  }
-
-  if (
-    v1Array.length > v2Array.length &&
-    v1Array.slice(l1).filter((x) => x).length
-  ) {
-    return 1;
-  }
-
-  if (
-    v1Array.length < v2Array.length &&
-    v2Array.slice(l2).filter((x) => x).length
-  ) {
-    return -1;
   }
 
   return 0;
