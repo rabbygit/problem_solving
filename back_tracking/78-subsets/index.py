7c4d70b756a8805a21364d2e0da633b4fafbd989
rabbygit <rabbyhossain482@gmail.com>
2024-05-21 20:56:18 +0600

    feat: added alternate solution of 78-subsets

--- back_tracking/78-subsets/index.py
+++ back_tracking/78-subsets/index.py
@@ -20,3 +20,23 @@ class Solution:
         backtrack([], 0)
 
         return result
+
+
+class Solution1:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(i: int, sub_res: List[int]):
+            if i == len(nums):
+                res.append(sub_res.copy())
+                return
+
+            # two choices, include and exclude the current number
+            sub_res.append(nums[i])
+            dfs(i + 1, sub_res)
+
+            sub_res.pop()
+            dfs(i + 1, sub_res)
+
+        dfs(0, [])
+        return res
