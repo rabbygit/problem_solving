04e82faf3e417d32817e55c39eb59b9d86b4ba2b
rabbygit <rabbyhossain482@gmail.com>
2024-07-12 00:10:03 +0600

    feat: added alternate solution of 1190-reverse-substrings-between-each-pair-of-parentheses

--- stack/1190-reverse-substrings-between-each-pair-of-parentheses/index.py
+++ stack/1190-reverse-substrings-between-each-pair-of-parentheses/index.py
@@ -5,9 +5,9 @@ class Solution:
         pair = {}
 
         for i, c in enumerate(s):
-            if c == '(':
+            if c == "(":
                 opened.append(i)
-            if c == ')':
+            if c == ")":
                 j = opened.pop()
                 pair[i], pair[j] = j, i
 
@@ -15,11 +15,33 @@ class Solution:
         i, d = 0, 1
 
         while i < len(s):
-            if s[i] in '()':
+            if s[i] in "()":
                 i = pair[i]
                 d = -d
             else:
                 res.append(s[i])
 
             i += d
-        return ''.join(res)
+        return "".join(res)
+
+
+class Solution1:
+    # T.C: O(n * m), m is the number of pairs and S.C: O(n)
+    def reverseParentheses(self, s):
+        stack = []
+
+        for c in s:
+            if c == ")":
+                reversed_word = []
+                while stack[-1] != "(":
+                    reversed_word.append(stack.pop())
+                
+                stack.pop() # remove the "("
+                
+                # append the reversed characters again to the stack
+                for rc in reversed_word:
+                    stack.append(rc)
+            else:
+                stack.append(c)
+
+        return "".join(stack)
