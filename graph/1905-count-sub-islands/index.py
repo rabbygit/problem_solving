3b29085f5d8e3b79d70cf6775e31dd99f4529053
rabbygit <rabbyhossain482@gmail.com>
2024-08-28 22:10:27 +0600

    fix: added comments

--- graph/1905-count-sub-islands/index.py
+++ graph/1905-count-sub-islands/index.py
@@ -2,9 +2,10 @@ from typing import List
 
 
 class Solution:
+    # T.C and S.C: O(m * n)
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         ROWS, COLS = len(grid1), len(grid1[0])
-        visited = set()
+        visited, count = set(), 0
 
         def dfs(r, c):
             if (
@@ -15,19 +16,21 @@ class Solution:
                 or grid2[r][c] == 0
                 or (r, c) in visited
             ):
+                # end of the island, return True
                 return True
 
             res = True
             if grid1[r][c] == 0:
+                # cell is missing in grid1, return False
                 res = False
 
             visited.add((r, c))
+            # recursively visit in 4 directions
             for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 res = dfs(r + dr, c + dc) and res
 
             return res
 
-        count = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid2[r][c] and (r, c) not in visited and dfs(r, c):
