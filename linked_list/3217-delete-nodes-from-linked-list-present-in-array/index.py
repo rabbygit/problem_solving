609ec5920a70f8fbaea3141b2fa33ef96af76e39
rabbygit <rabbyhossain482@gmail.com>
2024-09-06 22:20:42 +0600

    feat: added solution of 3217-delete-nodes-from-linked-list-present-in-array

---
+++ linked_list/3217-delete-nodes-from-linked-list-present-in-array/index.py
@@ -0,0 +1,30 @@
+import collections
+from typing import List, Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    # T.C: O(n+m) and S.C: O(n)
+    def modifiedList(
+        self, nums: List[int], head: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        numsMap = collections.Counter(nums)
+        prev = dummyHead = ListNode(0, head)
+        curr = head
+
+        while curr:
+            if curr.val in numsMap:
+                # skipping(deleting) the current node
+                prev.next = curr.next
+            else:
+                prev = curr
+
+            curr = curr.next
+
+        return dummyHead.next
