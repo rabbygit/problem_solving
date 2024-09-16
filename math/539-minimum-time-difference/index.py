05fe9664c2083174a5fdba59c816b76b825d872a
rabbygit <rabbyhossain482@gmail.com>
2024-09-16 11:42:18 +0600

    fix: updated solution of 539-minimum-time-difference

--- math/539-minimum-time-difference/index.py
+++ math/539-minimum-time-difference/index.py
@@ -1,27 +1,25 @@
 from typing import List
 
 
-# ref: https://leetcode.com/problems/minimum-time-difference/solutions/474787/python3-o-nlogn-time-with-detailed-explanation/
 class Solution:
 
     def findMinDifference(self, timePoints: List[str]) -> int:
-        res = float('inf')
-
-        for i in range(len(timePoints)):
-            timePoints[i] = self.convertToMin(timePoints[i])
+        # convert HH:MM to minute
+        for i, t in enumerate(timePoints):
+            h, m = map(int, t.split(":"))
+            timePoints[i] = h * 60 + m
 
         # sort the time point
         timePoints.sort()
 
+        # distance of the final point to it's first clockwise point
+        res = 24 * 60 - timePoints[-1] + timePoints[0]
+
         # calculate min diff of each timepoint clockwise
         for i in range(len(timePoints) - 1):
-            res = min(res, timePoints[i + 1] - timePoints[i])
-
-        # distance of the final point to it's first clockwise point
-        res = min(res, 60 * 24 - timePoints[-1] + timePoints[0])
+            diff = timePoints[i + 1] - timePoints[i]
+            if diff == 0:
+                return 0
+            res = min(res, diff)
 
         return res
-
-    def convertToMin(self, time: str):
-        h, m = time.split(':')
-        return int(h) * 60 + int(m)
